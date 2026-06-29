8cdaded79b720a791499aae35dd54cf9b60704ce
Ana Flavia <afcr1@aluno.ifnmg.edu.br>
2026-06-29 11:52:10 -0300

    storage

--- utils/storage.py
+++ utils/storage.py
@@ -2,11 +2,20 @@ from google.cloud import storage
 from google.oauth2 import service_account
 import streamlit as st
 
+BUCKET_NAME = "bucket-copa-ana-flavia"
+
 credentials = service_account.Credentials.from_service_account_info(
     st.secrets["gcp_service_account"]
 )
 
 client = storage.Client(
     credentials=credentials,
-    project=credentials.project_id,
+    project=credentials.project_id
 )
+
+bucket = client.bucket(BUCKET_NAME)
+
+
+def baixar_imagem(nome):
+    blob = bucket.blob(f"imagens_jogadores/{nome}")
+    return blob.download_as_bytes()
